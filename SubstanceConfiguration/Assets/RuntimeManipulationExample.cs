6e77393456439b1a78d073b3ad7a223a9891b465
Jason Watson <jbw@jbw.cc>
2016-02-05 23:38:33 +0000

    Example for manipulating config at runtime.

---
+++ SubstanceConfiguration/Assets/RuntimeManipulationExample.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RuntimeManipulationExample : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        var aCube = GameObject.FindGameObjectWithTag("Cube1");
+
+        var grassParams = aCube.GetComponent<GrassSubstanceParameters>();
+        grassParams.GrassAmount = 10;
+
+        var config = aCube.GetComponent<SubstanceConfiguration>();
+        config.parameters = grassParams;
+        config.Config();
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
